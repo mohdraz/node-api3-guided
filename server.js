0c9475faa2be3917dfac0dac68604fe6ea9e74b0
Mohd Raza <noorinfotechinc@gmail.com>
2020-01-22 22:13:57 -0600

    project

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = require('express'); // importing a CommonJS module
-
-const hubsRouter = require('./hubs/hubs-router.js');
+const express = require("express"); // importing a CommonJS module
+const hubsRouter = require("./hubs/hubs-router.js");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
 
-server.use(express.json());
+// server.use(express.json());
+// server.use(helmet());
+// server.use(morgan("dev"));
+// server.use(methodLogger);
 
-server.use('/api/hubs', hubsRouter);
+// server.use(express.json(), helmet(), morgan("dev"), methodLogger);
+const middleware = [express.json(), helmet(), morgan("dev"), methodLogger];
+server.use(middleware);
+server.use(addName);
+// server.use("/api/hubs", lockout);
+server.use(lockDivThree);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res, next) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
+  // next();
 
   res.send(`
     <h2>Lambda Hubs API</h2>
@@ -17,4 +29,32 @@ server.get('/', (req, res) => {
     `);
 });
 
+function methodLogger(req, res, next) {
+  console.log(`${req.method} Request`);
+  next();
+}
+
+function addName(req, res, next) {
+  req.name = req.name || "Raza";
+  next();
+}
+
+function lockout(req, res) {
+  // do some validation ....
+  // .. uh oh Batmaon
+  res.status(403).json({ message: "api lockout!" });
+}
+
+function lockDivThree(req, res, next) {
+  const currentDate = new Date();
+  // if (currentDate.getSeconds() % 3 === 0) {
+  //   res.status(403).json({ message: "Loced out cause div by 3" });
+  // } else {
+  //   next();
+  // }
+
+  currentDate.getSeconds() % 3 === 0
+    ? res.status(403).json({ message: "Loced out cause div by 3" })
+    : next();
+}
 module.exports = server;
